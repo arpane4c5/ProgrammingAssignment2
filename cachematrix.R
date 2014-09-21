776e5d16daa7fe2cfc62805a34d832a968aea89a
arpane4c5 <arpan.e4c5@gmail.com>
2014-09-21 23:03:18 +0530

    Second Commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,42 +1,54 @@
-## Put comments here that give an overall description of what your
+## makeCacheMatrix() : takes the matrix object x as parameter. 
+#### 1. setMatrix() : takes matrix object and sets x equal to mat
+#### 2. getMatrix() : returns the matrix object x
+#### 3. calculateMatInverse() : calculates inverse of matrix x and caches in invMat
+#### 4. 
 ## functions do
 
-## Write a short comment describing this function
+## makeCacheMatrix() : takes the matrix object x as parameter. 
 
 makeCacheMatrix <- function(x = matrix()) {
 
      invMat <- NULL
      
+     ## setMatrix() : takes matrix object and sets x equal to mat
      setMatrix <- function(mat){
           x <<- mat
           invMat <<- NULL
      }
      
+     ## getMatrix() : returns the matrix object x
      getMatrix <- function(){
           x
      }
      
+     ## calculateMatInverse() : calculates inverse of matrix x and caches in invMat
      calculateMatInverse <- function(){
           invMat <<- solve(x)
-          
+          invMat
      }
+     
+     getInverseMatrix <- function(){
+          invMat
+     } 
      
      setInverseMatrix <- function(inv){
           invMat <<- inv
      }
      
-     list(setMatrix = setmatrix, 
+     list(setMatrix = setMatrix, 
           getMatrix = getMatrix,
           calculateMatInverse = calculateMatInverse,
+          getInverseMatrix = getInverseMatrix,
           setInverseMatrix = setInverseMatrix)
 }
 
 
-## Write a short comment describing this function
+## 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-     inv <- x$getInv()
+     ## Return a matrix that is the inverse of 'x'
+     inv <- x$getInverseMatrix()
      if(!is.null(inv)){
           print("getting cached data...")
           return(inv)
